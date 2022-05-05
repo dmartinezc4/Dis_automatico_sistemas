a9865d0998e81381615a087af78227141f09f7a2
dmartinezc4 <43136464+dmartinezc4@users.noreply.github.com>
2022-05-05 13:19:01 +0200

    Programa HolaMundo
    Hola mundo de vitis

---
+++ practica4/HolaMundo/Debug/src/helloworld.d
@@ -0,0 +1,12 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/xil_printf.h \
+ C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/xil_types.h \
+ C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/xparameters.h \
+ C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/bspconfig.h
+../src/platform.h:
+../src/platform_config.h:
+C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/xil_printf.h:
+C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/xil_types.h:
+C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/xparameters.h:
+C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/bspconfig.h:
---
+++ practica4/HolaMundo/Debug/src/platform.d
@@ -0,0 +1,17 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/xparameters.h \
+ C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/xil_cache.h \
+ C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/mb_interface.h \
+ C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/xil_types.h \
+ C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/xil_assert.h \
+ C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/xil_exception.h \
+ C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/xparameters.h \
+ ../src/platform_config.h
+C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/xparameters.h:
+C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/xil_cache.h:
+C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/mb_interface.h:
+C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/xil_types.h:
+C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/xil_assert.h:
+C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/xil_exception.h:
+C\:/Users/david/Desktop/Nebrija/vitis-eclipse/practica_4/export/practica_4/sw/practica_4/standalone_domain/bspinclude/include/xparameters.h:
+../src/platform_config.h:
